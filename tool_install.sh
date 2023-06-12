3e5a045e7b37797f72524a6877d7ae69a5919dbe
0xstaark <112165759+0xstaark@users.noreply.github.com>
2023-06-12 14:06:51 +0200

    Update tool_install.sh
    Some minor changes

--- tool_install.sh
+++ tool_install.sh
@@ -5,42 +5,70 @@ GREEN="\e[32m"
 YELLOW="\e[33m"
 NC="\e[0m"
 
+echo -e ${YELLOW}"----------------------------------------------"
+echo -e "${GREEN}[+]${NC} ${YELLOW}Updateing system"
+echo -e "----------------------------------------------"${NC}
+#sudo apt-get -y update 2>/dev/null
 
-echo -e ${GREEN}"[+] Updateing system..."${NC}
-sudo apt-get -y update
+echo -e ${YELLOW}"----------------------------------------------"
+echo -e "${GREEN}[+]${NC} ${YELLOW}Unziping Rockyou.txt"
+echo -e "----------------------------------------------"${NC}
+gunzip /usr/share/wordlists/rockyou.txt.zip 2>/dev/null
 
-echo -e ${GREEN}"[+] unziping Rockyou.txt..."${NC}
-unzip -d usr/share/wordlists/rockyou.txt.zip
+echo -e ${YELLOW}"----------------------------------------------"
+echo -e "${GREEN}[+]${NC} ${YELLOW}Installing Terminator"
+echo -e "----------------------------------------------"${NC}
+sudo apt-get -y install terminator 2>/dev/null
 
-echo -e ${GREEN}"[+] Installing Terminator..."${NC}
-sudo apt-get -y install terminator
+echo -e ${YELLOW}"----------------------------------------------"
+echo -e "${GREEN}[+]${NC} ${YELLOW}Installing batcat"
+echo -e "----------------------------------------------"${NC}
+sudo apt-get -y install bat 2>/dev/null
 
-echo -e ${GREEN}"[+] installing batcat..."${NC}
-sudo apt-get -y install bat
+echo -e ${YELLOW}"----------------------------------------------"
+echo -e "${GREEN}[+]${NC} ${YELLOW}Installing command line history search"
+echo -e "----------------------------------------------"${NC}
+sudo git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install 2>/dev/null
 
-echo -e ${GREEN}"[+] Installing command line history search..."${NC}
-sudo git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+echo -e ${YELLOW}"----------------------------------------------"
+echo -e "${GREEN}[+]${NC} ${YELLOW}Installing Rustscan"
+echo -e "----------------------------------------------"${NC}
+sudo wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb && dpkg -i rustscan_2.0.1_amd64.deb 2>/dev/null
+echo -e ${YELLOW}"----------------------------------------------"
+echo -e "${GREEN}[+]${NC} ${YELLOW}Removing Rustscan install file"${NC}
+echo -e "${YELLOW}----------------------------------------------"${NC}
+rm rustscan_2.0.1_amd64.deb
 
-echo -e ${GREEN}"[+] Installing Rustscan..."${NC}
-sudo https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb && dpkg -i /home/$USER/downloads/rustscan_2.0.1_amd64.deb 
+echo -e ${YELLOW}"----------------------------------------------"
+echo -e "${GREEN}[+]${NC} ${YELLOW}Installing wfuzz"
+echo -e "----------------------------------------------"${NC}
+sudo apt-get -y install wfuzz 2>/dev/null
 
-echo -e ${GREEN}"[+] Installing wfuzz..."${NC}
-sudo apt-get -y install wfuzz
+echo -e ${YELLOW}"----------------------------------------------"
+echo -e "${GREEN}[+]${NC} ${YELLOW}Installing fuff"
+echo -e "----------------------------------------------"${NC}
+sudo apt-get -y install ffuf 2>/dev/null
 
-echo -e ${GREEN}"[+] Installing fuff..."${NC}
-sudo apt-get -y install ffuf
+echo -e ${YELLOW}"----------------------------------------------"
+echo -e "${GREEN}[+]${NC} ${YELLOW}Installing seclists"
+echo -e "----------------------------------------------"${NC}
+sudo apt-get -y install seclists 2>/dev/null
 
-echo -e ${GREEN}"[+] Installing seclists..."${NC}
-sudo apt-get -y install seclists
+echo -e ${YELLOW}"----------------------------------------------"
+echo -e "${GREEN}[+]${NC} ${YELLOW} Installing Bloodhound"
+echo -e "----------------------------------------------"${NC}
+sudo apt-get -y install bloodhound 2>/dev/null
 
-echo -e ${GREEN}"[+] Installing Bloodhound..."${NC}
-sudo apt-get -y install bloodhound
-
-echo -e ${GREEN}"[+] Installing Neo4j..."${NC}
-sudo apt-get -y install neo4j
+echo -e ${YELLOW}"----------------------------------------------"
+echo -e "${GREEN}[+]${NC} ${YELLOW} Installing Neo4j"
+echo -e "----------------------------------------------"${NC}
+sudo apt-get -y install neo4j 2>/dev/null
 echo ""
+echo -e ${GREEN}"----------------------------------------------"
+echo -e "[+] DONE!"
+echo -e "----------------------------------------------"${NC}
 echo ""
-echo -e ${YELLOW}"-------------------------------------------"
+echo -e ${YELLOW}"----------------------------------------------"
 echo "Add this to your .zshrc file"
 echo "Alias cat='batcat'"
-echo "-------------------------------------------"${NC}
+echo "----------------------------------------------"
