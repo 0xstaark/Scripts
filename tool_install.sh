8d640d52183abf88f22b2e27a12e9cf4014dc853
0xstaark <112165759+0xstaark@users.noreply.github.com>
2023-06-12 13:29:01 +0200

    Update tool_install.sh
    Added colors

--- tool_install.sh
+++ tool_install.sh
@@ -1,41 +1,46 @@
 #!/bin/bash
 
+#Colors
+GREEN="\e[32m"
+YELLOW="\e[33m"
+NC="\e[0m"
 
-echo "[+] Updateing system..."
+
+echo -e ${GREEN}"[+] Updateing system..."${NC}
 sudo apt-get -y update
 
-echo "unziping Rockyou.txt..."
+echo -e ${GREEN}"[+] unziping Rockyou.txt..."${NC}
 unzip -d usr/share/wordlists/rockyou.txt.zip
 
-echo "[+] Installing Terminator..."
+echo -e ${GREEN}"[+] Installing Terminator..."${NC}
 sudo apt-get -y install terminator
 
-echo "[+] installing batcat..."
+echo -e ${GREEN}"[+] installing batcat..."${NC}
 sudo apt-get -y install bat
 
-echo "[+] Installing command line history search..."
+echo -e ${GREEN}"[+] Installing command line history search..."${NC}
 sudo git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
-echo "[+] Installing Rustscan..."
-sudo https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb && dpkg -i rustscan_2.0.1_amd64.deb 
+echo -e ${GREEN}"[+] Installing Rustscan..."${NC}
+sudo https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb && dpkg -i /home/$USER/downloads/rustscan_2.0.1_amd64.deb 
 
-echo "[+] Installing wfuzz..."
+echo -e ${GREEN}"[+] Installing wfuzz..."${NC}
 sudo apt-get -y install wfuzz
 
-echo "[+] Installing fuff..."
+echo -e ${GREEN}"[+] Installing fuff..."${NC}
 sudo apt-get -y install ffuf
 
-echo "[+] Installing seclists..."
+echo -e ${GREEN}"[+] Installing seclists..."${NC}
 sudo apt-get -y install seclists
 
-echo "[+] Installing Bloodhound..."
+echo -e ${GREEN}"[+] Installing Bloodhound..."${NC}
 sudo apt-get -y install bloodhound
 
-echo "[+] Installing Neo4j..."
+echo -e ${GREEN}"[+] Installing Neo4j..."${NC}
 sudo apt-get -y install neo4j
 echo ""
 echo ""
-echo "-------------------------------------------"
+echo -e ${YELLOW}"-------------------------------------------"
 echo "Add this to your .zshrc file"
 echo "Alias cat='batcat'"
-echo "-------------------------------------------"
+echo "-------------------------------------------"${NC}
