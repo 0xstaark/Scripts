1b9f384a683809ccd2fbffef17022d7485fc690b
0xstaark <112165759+0xstaark@users.noreply.github.com>
2023-10-12 16:32:54 +0200

    Update tool_install.sh
    Minor changes and bug fix.

--- tool_install.sh
+++ tool_install.sh
@@ -12,7 +12,18 @@ LIGHT_BLUE="\e[38;5;39m"
 NC="\e[0m"
 
 user_name=$(whoami)
+user_name2=${SUDO_USER}
+user_home=$HOME
 startdir=$(pwd)
+
+user_home2=$(while IFS=: read -r user _ uid _ _ home _; do
+    if [[ $user == "$SUDO_USER" ]] && { [[ $uid -eq 0 ]] || [[ $uid -ge 1000 && $uid -lt 2000 ]]; }; then
+        echo -e "${home}"
+    fi
+done < /etc/passwd)
+
+# Display the result
+#echo "$user_home2"
 
 echo ""
 echo -e ${GREEN}" Created by:"
@@ -27,22 +38,36 @@ echo -e "${BLUE}https://github.com/0xstaark"${NC}
 echo ""
 
 ###########################################################################
+# System Update
+###########################################################################
+
+update_system() {
+echo -e "${YELLOW}[!] To update the system you need to run with SUDO"${NC}
+sudo -v
+sudo apt-get -qq -y update >/dev/null 2>&1
+echo -e "${GREEN}[!] System update complete"
+}
+
+###########################################################################
 # INSTALING TOOLLS
 ###########################################################################
 
 #Fuction for Installing tools, and then calling the function via the menu
 install_tools() {
-echo -e ${GREEN}"------------------------------------------"
+echo -e "${YELLOW}[!] To install the tools you need to run with SUDO"${NC}
+sudo -v
+echo -e ${GREEN}"-----------------------------------------------------"
 echo -e ${GREEN}"[*] Installing tools"
-echo -e ${GREEN}"------------------------------------------"
+echo -e ${GREEN}"-----------------------------------------------------"
 
 #Unpacking Rockyou.txt.gz
-if [[ $(ls /usr/share/wordlists | grep rock) == 'rockyou.txt' ]]; then
+if [[ $(ls /usr/share/wordlists | grep rockyou.txt) == 'rockyou.txt' ]]; then
 	printf "${GREEN}[+]${GREEN} %-20s %s\n" "Rockyou.txt" "Already unpacked"
 
 else
 	echo -e "${GREEN}[*]${YELLOW} Unpacking Rockyou.txt"
-	gunzip /usr/share/wordlists/rockyou.txt.gz >/dev/null 2>&1
+	gunzip /usr/share/wordlists/rockyou.txt.gz -y >/dev/null 2>&1
+	rm -r /usr/share/wordlists/rockyou.txt.gz
 fi
 
 #Installing seclists
@@ -70,15 +95,6 @@ if [[ $(which batcat) == '/usr/bin/batcat' ]]; then
 else
 	echo -e "${GREEN}[*]${YELLOW} Installing batcat"
 	sudo apt-get -qq -y install bat >/dev/null 2>&1
-fi
-
-# Installing Better command-line reverse search function
-if [[ $(which fzf) == /${user_name}'/.fzf/bin/fzf' ]]; then
-	printf "${GREEN}[+]${GREEN} %-20s %s\n" "Reverse command-line" "Already installed"
-
-else
-	echo -e "${GREEN}[*]${YELLOW} Installing better reverse command-line history search"
-	sudo git clone -q --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install >/dev/null 2>&1
 fi
 
 #Intalling Rustscan
@@ -155,96 +171,218 @@ echo -e "[+] ${BLUE}DONE!"${NC}
 
 #Fuction for dowloading scripts, and then calling the function via the menu
 download_scripts() {
-#Crerating tools directory
-tooldir="/${user_name}/tools"
-echo -e ${GREEN}"------------------------------------------"
-echo -e "${GREEN}[+] Creating Tools directory ${BLUE}[${tooldir}]${NC}"
-mkdir ${tooldir} 2>/dev/null
-cd ${tooldir}
-sleep 2
 
-echo -e ${GREEN}"------------------------------------------"
-echo -e ${GREEN}"[*] Downloading Scripts into ${BLUE}[${tooldir}]"
-echo -e ${GREEN}"------------------------------------------"
+# Default directory
+toolsdir="$HOME/tools"
+
+# Prompt the user for a custom directory
+echo -e "Choose directory to download Script to. Example ${YELLOW}/opt/tools${NC}"
+echo ""
+read -p "Enter the directory to use or ENTER for default: [${toolsdir}]: " userdir
+
+# If user provides input, use it as the tools directory
+if [[ -n "$userdir" ]]; then
+    toolsdir="$userdir"
+fi
+
+# Check if directory exists
+if [[ -d "$toolsdir" ]]; then
+    echo -e ${YELLOW}"[!] Using directory ${BLUE}[${toolsdir}]${YELLOW}"
+else
+    echo -e ${GREEN}"Creating directory: [${BLUE}${toolsdir}${GREEN}]${NC}"
+    mkdir -p "$toolsdir"
+fi
+
+# Change to the tools directory
+cd "$toolsdir"
+echo -e ${GREEN}"-----------------------------------------------------"
+echo -e ${GREEN}"[*] Downloading Scripts into ${BLUE}[${toolsdir}]"
+echo -e ${GREEN}"-----------------------------------------------------"
+sleep 1
+
 
 #Downloading mimikatz.exe
-echo -e "${GREEN}[+]${YELLOW} Downloading Mimikatz.exe${NC}"
-wget https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20220919/mimikatz_trunk.7z >/dev/null 2>&1
-7z e *.7z -y >/dev/null 2>&1
-rm -rf /root/tools/x64 >/dev/null
-rm -rf /root/tools/Win32 >/dev/null
-mv mimikatz.exe dog.exe
-rm -rf mimi*
-rm -rf kiwi*
-rm -rf README.md
-mv dog.exe mimikatz.exe
+if [[ ! -f "mimikatz.exe" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "Mimikatz.exe"
+	wget https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20220919/mimikatz_trunk.7z >/dev/null 2>&1
+	7z e *.7z -y >/dev/null 2>&1
+	rm -rf ${toolsdir}/x64 >/dev/null
+	rm -rf ${toolsdir}/Win32 >/dev/null
+	mv mimikatz.exe dog.exe
+	rm -rf mimi*
+	rm -rf kiwi*
+	rm -rf README.md
+	mv dog.exe mimikatz.exe
+else
+	printf "${GREEN}[x] %-15s %s\n" "mimikatz.exe" "already exists. Skipping download"
+fi
 
 #Downloading SharpHound.ps1
-echo -e "${GREEN}[+]${YELLOW} Downloading SharpHound.ps1${NC}"
-wget -q https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/SharpHound.ps1 >/dev/null 2>&1
+if [[ ! -f "SharpHound.ps1" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "SharpHound.ps1"
+	wget -q https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/SharpHound.ps1 >/dev/null 2>&1
+else
+	printf "${GREEN}[x] %-15s %s\n" "SharpHound.ps1" "already exists. Skipping download"
+fi
 
 #Downloading SharpHound.exe
-echo -e "${GREEN}[+]${YELLOW} Downloading SharpHound.exe${NC}"
-wget -q https://github.com/BloodHoundAD/BloodHound/raw/master/Collectors/SharpHound.exe >/dev/null 2>&1
+if [[ ! -f "SharpHound.exe" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "SharpHound.exe"
+	wget -q https://github.com/BloodHoundAD/BloodHound/raw/master/Collectors/SharpHound.exe >/dev/null 2>&1
+else
+	printf "${GREEN}[x] %-15s %s\n" "SharpHound.exe" "already exists. Skipping download"
+fi
 
 #Downloading winPEASx64.exe
-echo -e "${GREEN}[+]${YELLOW} Downloading winPEASx64.exe${NC}"
-wget -q https://github.com/carlospolop/PEASS-ng/releases/download/20230917-ec588706/winPEASx64.exe >/dev/null 2>&1
+if [[ ! -f "winPEASx64.exe" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "winPEASx64.exe"
+	wget -q https://github.com/carlospolop/PEASS-ng/releases/download/20230917-ec588706/winPEASx64.exe >/dev/null 2>&1
+else
+	printf "${GREEN}[x] %-15s %s\n" "winPEASx64.exe" "already exists. Skipping download"
+fi
 
 #Downloading winPEASany.exe
-echo -e "${GREEN}[+]${YELLOW} Downloading winPEASany.exe${NC}"
-wget -q https://github.com/carlospolop/PEASS-ng/releases/download/20230917-ec588706/winPEASany.exe >/dev/null 2>&1
+if [[ ! -f "winPEASany.exe" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "winPEASany.exe"
+	wget -q https://github.com/carlospolop/PEASS-ng/releases/download/20230917-ec588706/winPEASany.exe >/dev/null 2>&1
+else
+	printf "${GREEN}[x] %-15s %s\n" "winPEASany.exe" "already exists. Skipping download"
+fi
 
 #Downloading linPEAS.sh
-echo -e "${GREEN}[+]${YELLOW} Downloading linPEAS.sh${NC}"
-wget -q https://github.com/carlospolop/PEASS-ng/releases/download/20230917-ec588706/linpeas.sh >/dev/null 2>&1
+if [[ ! -f "linpeas.sh" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "linPEAS.sh"
+	wget -q https://github.com/carlospolop/PEASS-ng/releases/download/20230917-ec588706/linpeas.sh >/dev/null 2>&1
+else
+	printf "${GREEN}[x] %-15s %s\n" "linPEAS.sh" "already exists. Skipping download"
+fi
 
 #Downloading Powerview.ps1
-echo -e "${GREEN}[+]${YELLOW} Downloading PowerView.ps1${NC}"
-wget -q https://github.com/PowerShellMafia/PowerSploit/raw/master/Recon/PowerView.ps1 >/dev/nul 2>&1
+if [[ ! -f "PowerView.ps1" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "PowerView.ps1"
+	wget -q https://github.com/PowerShellMafia/PowerSploit/raw/master/Recon/PowerView.ps1 >/dev/null 2>&1
+else
+	printf "${GREEN}[x] %-15s %s\n" "PowerView.ps1" "already exists. Skipping download"
+fi
 
 #Downloading PowerUp.ps1
-echo -e "${GREEN}[+]${YELLOW} Downloading PowerUp.ps1${NC}"
-wget -q https://github.com/PowerShellMafia/PowerSploit/raw/master/Privesc/PowerUp.ps1 >/dev/null 2>&1
+if [[ ! -f "PowerUp.ps1" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "PowerUp.ps1"
+	wget -q https://github.com/PowerShellMafia/PowerSploit/raw/master/Privesc/PowerUp.ps1 >/dev/null 2>&1
+else
+	printf "${GREEN}[x] %-15s %s\n" "PowerUp.ps1" "already exists. Skipping download"
+fi
 
 #Downloading Rubeus.exe
-echo -e "${GREEN}[+]${YELLOW} Downloading Rubeus.exe${NC}"
-wget -q https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/Rubeus.exe >/dev/null 2>&1
+if [[ ! -f "Rubeus.exe" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "Rubeus.exe"
+	wget -q https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/Rubeus.exe >/dev/null 2>&1
+else
+	printf "${GREEN}[x] %-15s %s\n" "Rubeus.exe" "already exists. Skipping download"
+fi
 
 #Downloading Inveigh.exe
-echo -e "${GREEN}[+]${YELLOW} Downloading Inveigh.exe${NC}"
-wget -q https://github.com/Kevin-Robertson/Inveigh/releases/download/v2.0.10/Inveigh-net3.5-v2.0.10.zip >/dev/null 2>&1
-unzip -qq *.zip 2>/dev/null
-rm *.zip
-rm *.config
-rm *.pdb
+if [[ ! -f "Inveigh.exe" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "Inveigh.exe"
+	wget -q https://github.com/Kevin-Robertson/Inveigh/releases/download/v2.0.10/Inveigh-net3.5-v2.0.10.zip >/dev/null 2>&1
+	unzip -qq *.zip 2>/dev/null
+	rm *.zip
+	rm *.config
+	rm *.pdb
+else
+	printf "${GREEN}[x] %-15s %s\n" "Inveigh.exe" "already exists. Skipping download"
+fi
 
 #Downloading Invegih.ps1
-echo -e "${GREEN}[+]${YELLOW} Downloading Inveigh.ps1${NC}"
-wget -q https://github.com/Kevin-Robertson/Inveigh/raw/master/Inveigh.ps1
+if [[ ! -f "Inveigh.ps1" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "Inveigh.ps1"
+	wget -q https://github.com/Kevin-Robertson/Inveigh/raw/master/Inveigh.ps1
+else
+	printf "${GREEN}[x] %-15s %s\n" "Inveigh.ps1" "already exists. Skipping download"
+fi
 
 #Downloading Microsoft sysinternal PSTools
-echo -e "${GREEN}[+]${YELLOW} Downloading Microsoft Sysinternals PSTools${NC}"
-wget -q https://download.sysinternals.com/files/PSTools.zip
-unzip -qq *.zip 2>/dev/null
-rm *.zip
-echo -e "${GREEN}[+] ${BLUE}DONE!"${NC}
-echo ""
+if [[ ! -f "PsExec.exe" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "Microsoft Sysinternals PSTools"
+	wget -q https://download.sysinternals.com/files/PSTools.zip
+	unzip -qq *.zip 2>/dev/null
+	rm *.zip
+else
+	printf "${GREEN}[x] %-15s %s\n" "PSTools" "already exists. Skipping download"
+fi
+
+#Downloading AutoRecon and installing requrements.
+if [[ ! -d "AutoRecon" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "AutoRecon"
+	git clone https://github.com/Tib3rius/AutoRecon.git 2>/dev/null
+	pip install -r AutoRecon/requirements.txt >/dev/null 2>&1
+else
+	printf "${GREEN}[x] %-15s %s\n" "AutoRecon" "already exists. Skipping download"
+fi
+
+#Downloading nc64.exe
+if [[ ! -f "nc64.exe" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "nc64.exe"
+	wget -q https://github.com/int0x33/nc.exe/raw/master/nc64.exe
+else
+	printf "${GREEN}[x] %-15s %s\n" "nc64.exe" "already exists. Skipping download"
+fi
+
+#Downloading nc.exe
+if [[ ! -f "nc.exe" ]]; then
+	eprintf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "nc.exe"
+	wget -q https://github.com/int0x33/nc.exe/raw/master/nc.exe
+else
+	printf "${GREEN}[x] %-15s %s\n" "nc.exe" "already exists. Skipping download"
+fi
+
+#Downloading PlumHound.py
+if [[ ! -f "PlumHound.py" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "PlumHound.py"
+	wget -q https://github.com/PlumHound/PlumHound/raw/master/PlumHound.py
+else
+	printf "${GREEN}[x] %-15s %s\n" "PlumHound.py" "already exists. Skipping download"
+fi
+
+#Downloadning pspy32
+if [[ ! -f "pspy32" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "pspy32"
+	wget -q https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy32
+else
+	printf "${GREEN}[x] %-15s %s\n" "pspy32" "already exists. Skipping download"
+fi
+
+#Downloading pspy64
+if [[ ! -f "pspy64" ]]; then
+	printf "${GREEN}[+]${YELLOW} %-15s %s\n" "Downloading" "pspy64"
+	wget -q https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64
+else
+	printf "${GREEN}[x] %-15s %s\n" "pspy64" "already exists. Skipping download"
+fi
+
+
+
+
+
+
+echo -e "${BLUE}[!] ${BLUE}DONE!"${NC}
+echo -e ${GREEN}"-----------------------------------------------------"
+rm *.txt 2>/dev/null
+rm *.chm 2>/dev/null
 cd ${startdir}
-}
+
+###########################################################################
+# Custom HTTP server from the tools directory
+###########################################################################
 
 #Adding custom server to server tools to ~/.bashrc
-custom_server() {
-
-
 if [[ $(cat ~/.zshrc | grep -o 'servtools()') == 'servtools()' ]]; then
 	echo -e "${GREEN}[+]${GREEN} servtools already installed"
+	echo -e "${YELLOW}[!] To use this server, reopen terminal and type servetools <port>"
 
 else
-	echo -e "${GREEN}[*]${YELLOW} Adding servtools to ${user_name}/.zshrc file}"
-
-	echo -e ${GREEN}"----------------------------------------------"
-	echo -e "${GREEN}[+]${YELLOW} Adding servtools script to: ${BLUE}~/.zshrc"${NC}
+	echo -e "${YELLOW}[!] Adding a custom server for starting HTTP.server form the tools directory"
+	echo -e "${YELLOW}[!] Adding servtools to ${user_home}/.zshrc file}"
 	echo "" >> ~/.zshrc 2>/dev/null
 	echo "" >> ~/.zshrc 2>/dev/null
 	echo "" >> ~/.zshrc 2>/dev/null
@@ -257,9 +395,9 @@ else
 	echo '    GREEN="\e[32m"' >> ~/.zshrc 2>/dev/null
 	echo '    NC="\e[0m"' >> ~/.zshrc 2>/dev/null
 	echo "    PORT=\$1" >> ~/.zshrc 2>/dev/null
-	echo '    DIR="/root/tools"' >> ~/.zshrc 2>/dev/null
+	echo '    DIR="mySuperuniqvalue"' >> ~/.zshrc 2>/dev/null
 	echo "    IP=\$(ip -4 addr show tun0 | grep -oP \"(?<=inet ).*(?=/)\")" >> ~/.zshrc 2>/dev/null
-	echo '    echo -e "${GREEN}Files in serving directory [${DIR}]${NC}"' >> ~/.zshrc 2>/dev/null
+	echo '    echo -e "${GREEN}Files in directory ${BLUE}[${DIR}]${NC}"' >> ~/.zshrc 2>/dev/null
 	echo '    ls ${DIR}' >> ~/.zshrc 2>/dev/null
 	echo '    echo -e "${GREEN}-------------------------------------------------------------------------${NC}"' >> ~/.zshrc 2>/dev/null
 	echo "    echo -e \"[+] Starting HTTP server from \${GREEN}[\$DIR]\${NC} on \$PORT\"" >> ~/.zshrc 2>/dev/null
@@ -272,62 +410,46 @@ else
 	echo "alias cat='batcat'" >> ~/.zshrc 2>/dev/null
 	source ~/.zshrc 2>/dev/null
 fi
+sed -i 's@DIR="mySuperuniqvalue"@DIR="'${toolsdir}'"@' ~/.zshrc
 }
 
-#Check if the user is running as root, if not exit and telle the user run the script with sudo.
-if [[ $EUID -ne 0 ]]; then
-   echo -e "${BLUE}You need to run this script with sudo${NC}"
-   exit 1
-fi
 
-# Ask the user if they want to update the system
-echo -e "${GREEN}[?]${YELLOW} Do you want to update the system? (y/N)${NC}"
-read -r update_choice
 
-if [[ $update_choice == "y" || $update_choice == "Y" ]]; then
-    echo -e "${GREEN}[*]${YELLOW} Updating System${NC}"
-    sudo apt-get -qq -y update 2>/dev/null 2>&1
-    
-    # After update is complete, ask if they want to continue
-    echo -e "${GREEN}[?]${YELLOW} Update complete! Do you want to continue? (y/N)${NC}"
-    read -r continue_choice
-    
-    if [[ $continue_choice != "y" && $continue_choice != "Y" ]]; then
-        echo -e "${GREEN}[+]${YELLOW} Exiting script${NC}"
-        exit 0
-    fi
-    
-else
-    echo -e "${GREEN}[*]${YELLOW} Skipping System Update${NC}"
-    echo ""
-fi
+###########################################################################
+# Menu Function
+###########################################################################
 
 # Function to display menu and prompt for user's choice.
 menu_choice() {
-    echo -e "${GREEN}[*] ${BLUE}Choose an option:${NC}"
-    echo -e "${GREEN}[1]${YELLOW} Install Tools${NC}"
-    echo -e "${GREEN}[2]${YELLOW} Download Scripts${NC}"
-    echo -e "${GREEN}[3]${YELLOW} Install Custom server${NC}"
-    echo -e "${GREEN}[4]${YELLOW} Install all above${NC}"
+    echo -e "${GREEN}[?]${BLUE} Choose an option:${NC}"
+    echo -e "${GREEN}[1]${YELLOW} Update System${NC}"
+    echo -e "${GREEN}[2]${YELLOW} Install Tools${NC}"
+    echo -e "${GREEN}[3]${YELLOW} Download Scripts${NC}"
+    echo -e "${GREEN}[4]${YELLOW} Install tools and Download scripts${NC}"
+    echo -e "${GREEN}[5]${YELLOW} All the above${NC}"
     echo -e "${GREEN}[0]${YELLOW} Exit${NC}"
 
-    read -p "Enter choice [1-4]: [0] To Exit: " choice
+    read -p "Enter choice [1-5]: [0] To Exit: " choice
 
     case $choice in
-        1) install_tools ;;
-        2) download_scripts ;;
-        3) custom_server ;;
+    	1) update_system ;;
+        2) install_tools ;;
+        3) download_scripts ;;
         4) 
             install_tools
             download_scripts
-            custom_server
+            ;;
+	5)  
+	    update_system 
+	    install_tools
+            download_scripts
             ;;
         0) 
             echo -e "${GREEN}[+] Exiting...${NC}"
             exit 0
             ;;
         *) 
-            echo -e "${RED}[!] Invalid option. Please choose a number between 0-4.${NC}"
+            echo -e "${RED}[!] Invalid option. Please choose a number between 0-5.${NC}"
             menu_choice
             ;;
     esac
