210ff121588e6befa63f1503c8ab996de9ae0cc2
0xstaark <112165759+0xstaark@users.noreply.github.com>
2025-01-08 10:57:55 +0100

    Update sun_tzu.sh
    minor changes

--- sun_tzu.sh
+++ sun_tzu.sh
@@ -1,1 +1,1 @@
-curl -s https://raw.githubusercontent.com/0xstaark/MISC/main/sun_tzu_quotes | shuf -n 1 | awk -F"\n" '{print $1}'
+curl -s https://raw.githubusercontent.com/0xstaark/MISC/main/sun_tzu_quotes | shuf -n 1 | awk -F"\n" '{print $1 "\n\n-Sun Tzu"}'
